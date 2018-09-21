c878968ae6c9c9bcbc73fcc26bef45ddc76b7812
boomvausstat <steliftw@gmail.com>
2018-09-21 14:47:02 -0400

    Moved headers to js

--- Desktop/myApp/web/cal.js
+++ Desktop/myApp/web/cal.js
@@ -9,26 +9,35 @@ var tday = today.getDay();
 var tmonth = today.getMonth();
 var tyear = 1900 + today.getYear();
 
+for(i = 0; i < 7; i++){
+	var row = document.createElement("div");
+	row.className = "row";
+	document.getElementById("calendar").appendChild(row);
+	if (i == 0){
+		for(j = 0; j < 7; j++){
+			var days = document.createElement("span");
+			days.className = "days";
+			document.getElementsByClassName("row")[0].appendChild(days);
+		}
+	}
+	else {
+		for(j=0;j<7;j++){
+			var box = document.createElement("span");
+			box.className = "box";
+			var date = document.createElement("span");
+			date.className = "date";
+			if(j+1+((i-1)*7) == tdate) date.id = "current";
+			document.getElementsByClassName("row")[i].appendChild(box);
+			document.getElementsByClassName("box")[j+((i-1)*7)].appendChild(date);
+			document.getElementsByClassName("date")[j+((i-1)*7)].innerHTML = j+1+((i-1)*7);
+		}
+	}
+}
+
 for(i=0;i<7;i++){
 	document.getElementsByClassName("days")[i].innerHTML = dayName[i];
 }	
 
-for(i = 0; i < 6; i++){
-	var row = document.createElement("div");
-	row.className = "row";
-	document.getElementById("calendar").appendChild(row);
-	for(j=0;j<7;j++){
-		var box = document.createElement("span");
-		box.className = "box";
-		var date = document.createElement("span");
-		date.className = "date";
-		if(j+1+(i*7) == tdate) date.id = "current";
-		document.getElementsByClassName("row")[i+1].appendChild(box);
-		document.getElementsByClassName("box")[j+(i*7)].appendChild(date);
-		document.getElementsByClassName("date")[j+(i*7)].innerHTML = j+1+(i*7);
-	}
-}
-
-document.getElementById("header").innerHTML = monthName[month];
+document.getElementById("header").innerHTML = monthName[tmonth];
 	
 document.getElementById("calendar").appendChild(document.createElement("br"));
