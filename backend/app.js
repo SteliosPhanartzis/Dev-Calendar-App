1966b8337ef6bfc932957a6b5e958fefb0918b39
Jacky Lau <jackylau.email@gmail.com>
2020-02-24 16:24:06 -0500

    added routes

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,15 @@ var path = require('path');
 
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
 //Bodyparser
 app.use(express.urlencoded({ extended: false }));
 
 //Routes
 app.use('/', require('./routes/index.js'));
-app.use(express.static(__dirname + '/public'));
-app.set('view engine', 'ejs');
+app.use('/calendar', require('./routes/calendar.js'))
 // app.use('/users', require('./routes/users'));
 
 //Define PORT
--- backend/routes/calendar.js
+++ backend/routes/calendar.js
@@ -1,7 +1,66 @@
 const express = require('express');
 const router =  express.Router();
 
-//Welcome page
-router.get('/', (req,res) => res.send('Calendar'));
+//Dashboard
+router.get('/', (req, res) => {
+	res.render('dashboard');
+})
+
+router.post('/', (req, res) => {
+	res.render('dashboard');
+})
+
+router.put('/', (req, res) => {
+	res.render('dashboard');
+})
+
+router.delete('/', (req, res) => {
+	res.render('dashboard');
+})
+
+//Login
+router.get('/', (req, res) => {
+	res.render('login');
+})
+
+router.post('/', (req, res) => {
+	res.render('login');
+})
+
+//Logout
+router.post('/', (req, res) => {
+	res.render('logout');
+})
+
+//Register
+router.get('/', (req, res) => {
+	res.render('register');
+})
+
+router.post('/', (req, res) => {
+	res.render('register');
+})
+
+//Friends
+router.get('/', (req, res) => {
+	res.render('friends');
+})
+
+router.post('/', (req, res) => {
+	res.render('friends');
+})
+
+router.put('/', (req, res) => {
+	res.render('friends');
+})
+
+router.delete('/', (req, res) => {
+	res.render('friends');
+})
+
+//Schedule
+router.get('/', (req, res) => {
+	res.render('schedule');
+})
 
 module.exports = router;
